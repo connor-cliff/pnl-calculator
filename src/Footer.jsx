ef5727c6bcccf91af68ea8ff4b335db102104ba8
connor <connorcliff211@gmail.com>
2024-10-10 09:29:52 +0100

    git commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,9 @@ function Footer(){
 
     return(
         <footer>
-            
+            <p>&copy; {new Date().getFullYear} Profit & Loss Calculator</p>
         </footer>
     );
 }
+
+export default Footer;
