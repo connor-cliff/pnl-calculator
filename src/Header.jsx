497fc87332bddfb9b5187101f2964c168ca9fe02
connor <connorcliff211@gmail.com>
2024-10-10 09:22:49 +0100

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+function Header(){
+
+    return(
+        <>
+        <header>
+            <h1>Profit & Loss Calculator</h1>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        </>
+    )
+}
+
+export default Header;
